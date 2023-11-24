cc5e040dda3443282d79646ab0b0111ce70a5ba7
Manik Sharma <90952302+Wmlster@users.noreply.github.com>
2023-11-24 21:38:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let div=document.CreateElement("div");
+div.id="browser-info";
+
+document.body.append(div);
+
+div.innerHTML="You are using "+navigator.appName+" version "+navigator.appVersion;
+
